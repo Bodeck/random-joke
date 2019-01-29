b0eea7e44343a9335d51146590c8384d0abc85ad
Bogus Franczak <bfranczak@gmail.com>
2019-01-29 20:26:20 +0100

    Add exception handler

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var url = 'http://api.icndb.com/jokes/random';
-document.getElementById('get-joke').addEventListener('click', function(){
-    getJoke();
-});
-document.addEventListener('HTMLContentLoaded', getJoke());
+// url = 'https://www.google.com/costam';
 var paragraph = document.getElementById('joke');
+document.getElementById('get-joke').addEventListener('click', getJoke);
+document.addEventListener('HTMLContentLoaded', getJoke());
 
 function getJoke() {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function() {
-        var response = JSON.parse(xhr.response);
-        paragraph.innerHTML = response.value.joke;
-    })
+    xhr.open('GET', prefix + url);
+    xhr.addEventListener('load', function () {
+        try {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.response);
+                paragraph.innerHTML = response.value.joke;
+            } else {
+                throw 'Oops! Something went wrong, I could not get joke!'
+            }
+        } catch (error) {
+            alert(error);
+        }
+    });
+
     xhr.send();
-}
+} 
